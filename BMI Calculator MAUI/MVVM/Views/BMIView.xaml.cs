5b1e8140b69061dcbf2c60bb582a5e7816a5e3f4
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-01 00:10:14 +0300

    Updated UI Code-behind file.

--- BMI Calculator MAUI/MVVM/Views/BMIView.xaml.cs
+++ BMI Calculator MAUI/MVVM/Views/BMIView.xaml.cs
@@ -1,9 +1,12 @@
+using BMI_Calculator_MAUI.MVVM.ViewModels;
+
 namespace BMI_Calculator_MAUI.MVVM.Views;
 
 public partial class BMIView : ContentPage
 {
-	public BMIView()
-	{
-		InitializeComponent();
-	}
+    public BMIView()
+    {
+        InitializeComponent();
+        BindingContext = new BMIViewModel();
+    }
 }
