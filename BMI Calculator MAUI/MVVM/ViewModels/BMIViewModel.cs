282a833ec200a46fe39543470c9a13418ebb033f
Micheal Shodamola <michealshodamola01@gmail.com>
2024-06-01 00:09:21 +0300

    Added BMI ViewModel. Instantiated BMI and set initial value.

---
+++ BMI Calculator MAUI/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BMI_Calculator_MAUI.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+
+        public Models.BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new Models.BMI();
+            BMI.Height = 180;
+            BMI.Weight = 73;
+        }
+
+    }
+}
